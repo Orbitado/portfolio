64d30c27e72dc33f94782367e6d9dc501fbb4af4
Orbitadoo <juanpaavaldezz@gmail.com>
2024-11-13 17:58:12 -0300

    feat/navbar:mobile-menu

--- src/app/componenjs/specific/NavBar.js
+++ src/app/componenjs/specific/NavBar.js
@@ -1,4 +1,5 @@
 "use strict";
+"use client";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
@@ -7,10 +8,22 @@ exports.default = void 0;
 var _link = _interopRequireDefault(require("next/link"));
 var _menuData = require("@/app/constants/menuData");
 var _types = require("@/app/types/types");
-var _lucideReact = require("lucide-react");
 var _ThemeSwitcher = _interopRequireDefault(require("./ThemeSwitcher"));
+var _LanguageToggler = _interopRequireDefault(require("./LanguageToggler"));
+var _lucideReact = require("lucide-react");
+var _react = require("react");
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
+function _slicedToArray(r, e) { return _arrayWithHoles(r) || _iterableToArrayLimit(r, e) || _unsupportedIterableToArray(r, e) || _nonIterableRest(); }
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+function _unsupportedIterableToArray(r, a) { if (r) { if ("string" == typeof r) return _arrayLikeToArray(r, a); var t = {}.toString.call(r).slice(8, -1); return "Object" === t && r.constructor && (t = r.constructor.name), "Map" === t || "Set" === t ? Array.from(r) : "Arguments" === t || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(t) ? _arrayLikeToArray(r, a) : void 0; } }
+function _arrayLikeToArray(r, a) { (null == a || a > r.length) && (a = r.length); for (var e = 0, n = Array(a); e < a; e++) n[e] = r[e]; return n; }
+function _iterableToArrayLimit(r, l) { var t = null == r ? null : "undefined" != typeof Symbol && r[Symbol.iterator] || r["@@iterator"]; if (null != t) { var e, n, i, u, a = [], f = !0, o = !1; try { if (i = (t = t.call(r)).next, 0 === l) { if (Object(t) !== t) return; f = !1; } else for (; !(f = (e = i.call(t)).done) && (a.push(e.value), a.length !== l); f = !0); } catch (r) { o = !0, n = r; } finally { try { if (!f && null != t.return && (u = t.return(), Object(u) !== u)) return; } finally { if (o) throw n; } } return a; } }
+function _arrayWithHoles(r) { if (Array.isArray(r)) return r; }
 function NavBar() {
+  var _useState = (0, _react.useState)(false),
+    _useState2 = _slicedToArray(_useState, 2),
+    isMenuOpen = _useState2[0],
+    setIsMenuOpen = _useState2[1];
   return /*#__PURE__*/React.createElement("header", {
     className: "container"
   }, /*#__PURE__*/React.createElement("nav", {
@@ -20,7 +33,7 @@ function NavBar() {
   }, /*#__PURE__*/React.createElement(_link.default, {
     href: "/"
   }, "LV")), /*#__PURE__*/React.createElement("ul", {
-    className: "nav__list"
+    className: "nav__list ".concat(isMenuOpen ? "open" : "close")
   }, _menuData.navbarLinks.map(function (link) {
     return /*#__PURE__*/React.createElement("li", {
       key: link.name,
@@ -29,11 +42,20 @@ function NavBar() {
       href: link.url,
       className: "nav__link"
     }, link.name));
-  }), /*#__PURE__*/React.createElement(_ThemeSwitcher.default, null), /*#__PURE__*/React.createElement("li", {
-    className: "nav__item"
-  }, /*#__PURE__*/React.createElement("div", {
-    className: "nav__toggler"
-  }, /*#__PURE__*/React.createElement(_lucideReact.Globe, {
+  })), /*#__PURE__*/React.createElement("ul", {
+    className: "nav__list--togglers"
+  }, /*#__PURE__*/React.createElement(_ThemeSwitcher.default, null), /*#__PURE__*/React.createElement(_LanguageToggler.default, null), /*#__PURE__*/React.createElement("li", {
+    className: "nav__item--mobile"
+  }, /*#__PURE__*/React.createElement("button", {
+    type: "button",
+    "aria-label": "Toggle navigation",
+    className: "nav__toggler",
+    onClick: function onClick() {
+      return setIsMenuOpen(!isMenuOpen);
+    }
+  }, isMenuOpen ? /*#__PURE__*/React.createElement(_lucideReact.X, {
+    className: "nav__icon"
+  }) : /*#__PURE__*/React.createElement(_lucideReact.Menu, {
     className: "nav__icon"
   }))))));
 }
--- src/app/components/specific/NavBar.tsx
+++ src/app/components/specific/NavBar.tsx
@@ -1,16 +1,20 @@
+"use client";
 import Link from "next/link";
 import { navbarLinks } from "@/app/constants/menuData";
 import { NavLinkProps } from "@/app/types/types";
-import { Globe } from "lucide-react";
 import ThemeSwitcher from "./ThemeSwitcher";
+import LanguageToggler from "./LanguageToggler";
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
 function NavBar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <header className="container">
       <nav className="nav">
         <h1 className="nav__logo">
           <Link href="/">LV</Link>
         </h1>
-        <ul className="nav__list">
+        <ul className={`nav__list ${isMenuOpen ? "open" : "close"}`}>
           {navbarLinks.map((link: NavLinkProps) => (
             <li key={link.name} className="nav__item">
               <Link href={link.url} className="nav__link">
@@ -18,11 +22,23 @@ function NavBar() {
               </Link>
             </li>
           ))}
+        </ul>
+        <ul className="nav__list--togglers">
           <ThemeSwitcher />
-          <li className="nav__item">
-            <div className="nav__toggler">
-              <Globe className="nav__icon" />
-            </div>
+          <LanguageToggler />
+          <li className="nav__item--mobile">
+            <button
+              type="button"
+              aria-label="Toggle navigation"
+              className="nav__toggler"
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
+            >
+              {isMenuOpen ? (
+                <X className="nav__icon" />
+              ) : (
+                <Menu className="nav__icon" />
+              )}
+            </button>
           </li>
         </ul>
       </nav>
