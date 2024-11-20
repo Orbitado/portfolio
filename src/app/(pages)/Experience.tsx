739ca45291f47593135f7f87d8126eb1bed2c833
Orbitadoo <juanpaavaldezz@gmail.com>
2024-11-20 11:30:18 -0300

    layout/experience: added timeline and do it scalable

--- src/app/(pages)/Experience.tsx
+++ src/app/(pages)/Experience.tsx
@@ -1,4 +1,5 @@
 import { BriefcaseBusiness } from "lucide-react";
+import { experienceList } from "@/constants/experienceList";
 import React from "react";
 
 function Experience() {
@@ -8,25 +9,24 @@ function Experience() {
         <BriefcaseBusiness className="experience__icon" aria-hidden="true" />
         Experience
       </h2>
-      <article>
+      <article className="experience__content">
         <ol className="experience__list">
-          <li className="experience__item">
-            <div>
-              <h3 className="experience__role">WebUI Developer</h3>
-              <h4 className="experience__company">Coppel</h4>
-              <time className="experience__date">2023 - Present</time>
-            </div>
-            <div>
-              <p className="experience__description">
-                As a full-stack developer, I have had the opportunity to work on
-                a wide range of projects, from small-scale applications to
-                complex web applications. My primary focus has been on
-                developing user interfaces using React, TypeScript, and Next.js,
-                while also contributing to the backend development using
-                Node.js, Express.js, and MongoDB.
-              </p>
-            </div>
-          </li>
+          {experienceList.map(
+            ({ company, position, startDate, endDate, description }) => (
+              <li key={company} className="experience__item">
+                <div className="experience__header">
+                  <h3 className="experience__role">{position}</h3>
+                  <h4 className="experience__company">{company}</h4>
+                  <time className="experience__date">
+                    {startDate} - {endDate}
+                  </time>
+                </div>
+                <div className="experience__brief">
+                  <p className="experience__description">{description}</p>
+                </div>
+              </li>
+            )
+          )}
         </ol>
       </article>
     </section>
---
+++ src/constants/experienceList.ts
@@ -0,0 +1,29 @@
+export const experienceList = [
+  {
+    company: "Cognizant",
+    position: "Frontend Developer",
+    startDate: "2022",
+    endDate: "Present",
+    description: [
+      "As a freelance Frontend Developer, I have worked on dynamic web applications and full-stack solutions. I have focused on delivering high-quality, scalable products that meet client requirements through technical expertise and efficient problem-solving. Currently, I am focused on creating SaaS applications, where I design user-friendly interfaces and ensure robust, scalable backend architectures.",
+    ],
+  },
+  {
+    company: "Cognizant",
+    position: "Frontend Developer",
+    startDate: "2022",
+    endDate: "Present",
+    description: [
+      "As a freelance Frontend Developer, I have worked on dynamic web applications and full-stack solutions. I have focused on delivering high-quality, scalable products that meet client requirements through technical expertise and efficient problem-solving. Currently, I am focused on creating SaaS applications, where I design user-friendly interfaces and ensure robust, scalable backend architectures.",
+    ],
+  },
+  {
+    company: "Cognizant",
+    position: "Frontend Developer",
+    startDate: "2022",
+    endDate: "Present",
+    description: [
+      "As a freelance Frontend Developer, I have worked on dynamic web applications and full-stack solutions. I have focused on delivering high-quality, scalable products that meet client requirements through technical expertise and efficient problem-solving. Currently, I am focused on creating SaaS applications, where I design user-friendly interfaces and ensure robust, scalable backend architectures.",
+    ],
+  },
+];
