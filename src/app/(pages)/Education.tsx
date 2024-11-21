c77dfbef4331ae801b98b4bfde8104b39dc61185
Orbitadoo <juanpaavaldezz@gmail.com>
2024-11-21 16:59:07 -0300

    feat/layout/education: added heading and icon. improved styles

--- src/app/(pages)/Education.tsx
+++ src/app/(pages)/Education.tsx
@@ -1,9 +1,13 @@
+import { GraduationCap } from "lucide-react";
 import React from "react";
 
 function Education() {
   return (
     <section id="education" className="section">
-      <h3 className="section__heading">Education</h3>
+      <h3 className="section__heading">
+        <GraduationCap className="section__icon" aria-hidden="true" />
+        Education
+      </h3>
     </section>
   );
 }
