028022d859731483a253142d9665a0bd79a98fd5
Orbitadoo <juanpaavaldezz@gmail.com>
2024-11-23 12:07:56 -0300

    layouting/education: added the html tags to init the page of educatino

--- src/app/(pages)/Education.tsx
+++ src/app/(pages)/Education.tsx
@@ -8,6 +8,24 @@ function Education() {
         <GraduationCap className="section__icon" aria-hidden="true" />
         Education
       </h3>
+      <article className="section__content">
+        <ul className="education__list">
+          <li className="education__item">
+            <h4 className="education__course">
+              Bachelor in Software Development and Quality
+            </h4>
+            <h5 className="education__institution">
+              University of the North Saint Thomas Aquinas
+            </h5>
+            <time className="education__date">2024 - 2026</time>
+            <p className="education__description">
+              Focused on web technologies and modern software development
+              practices. Key courses included Web Development, User Interface
+              Design, and Advanced JavaScript Programming.
+            </p>
+          </li>
+        </ul>
+      </article>
     </section>
   );
 }
